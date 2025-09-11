35cf67b10b7242afddd6ab16e72e8c961961ad3e
VitorMoreira2344 <cl205068@g.unicamp.br>
2025-09-11 12:01:25 -0300

    Update main.py
    trocando o meio de abrir a sala do futuro

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ async def main():
     redacao_texto = dados["redacao"]
 
     async with async_playwright() as pw:
-        navegador = await pw.chromium.launch(headless=False)
+        navegador = await pw.chromium.launch(channel="chrome", headless=False)
         pagina = await navegador.new_page()
         await pagina.goto("https://saladofuturo.educacao.sp.gov.br/escolha-de-perfil")
 
@@ -71,3 +71,4 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
